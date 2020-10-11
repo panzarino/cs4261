3f3fb03c0728a42e40d110523e1e54e5592e81c2
Zach Panzarino <zachary@panzarino.com>
2020-10-11 07:35:38 -0400

    Select courses for schedule

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import {
   IonBackButton,
   IonButtons,
@@ -18,18 +19,54 @@ import firebase from '../lib/firebase'
 
 import VerifyLoggedIn from '../components/VerifyLoggedIn'
 
+const db = firebase.database()
+
 const Add: React.FC = () => {
+  const history = useHistory()
   const [user] = useAuthState(firebase.auth())
   const [search, setSearch] = useState<string>('')
   const [courses, loading] = useList(
-    firebase
-      .database()
+    db
       .ref('courses')
       .orderByChild('name')
       .startAt(search.toUpperCase())
       .endAt(search.toUpperCase() + '\uf8ff')
       .limitToFirst(10)
   )
+  const [selections] = useList(
+    db
+      .ref('selections')
+      .orderByChild('uid')
+      .equalTo(user ? user.uid + '' : '')
+  )
+
+  const addCourse = async (courseKey: string) => {
+    if (!user) {
+      return
+    }
+
+    if (selections && selections.length > 0) {
+      const selection = selections[0]
+
+      if (selection.val().courses.includes(courseKey)) {
+        return
+      }
+
+      await db.ref(`selections/${selection.key}`).update({
+        uid: selection.val().uid,
+        courses: [...selection.val().courses, courseKey],
+      })
+    } else {
+      await db.ref('selections').push({
+        uid: user.uid,
+        courses: [courseKey],
+      })
+    }
+
+    setSearch('')
+
+    history.goBack()
+  }
 
   return (
     <IonPage>
@@ -51,7 +88,16 @@ const Add: React.FC = () => {
             !loading &&
             courses &&
             (courses.length > 0 ? (
-              courses.map((course) => <IonItem key={course.key}>{course.val().name}</IonItem>)
+              courses
+                .filter(
+                  (course) =>
+                    !selections || selections.length === 0 || !selections[0].val().courses.includes(course.key)
+                )
+                .map((course) => (
+                  <IonItem key={course.key} button={true} onClick={() => addCourse(course.key!)}>
+                    {course.val().name}
+                  </IonItem>
+                ))
             ) : (
               <IonItem key="none">No courses found!</IonItem>
             ))}
